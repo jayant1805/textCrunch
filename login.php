a89b3a0f652532d9e18055ad06db36bab962629c
Jayant Arora <j5arora@uwaterloo.ca>
2019-03-29 22:26:17 -0400

    Changed index.html to index.php

--- login.php
+++ login.php
@@ -60,7 +60,7 @@
         <div class="header-wrap">
           <div class="header-top d-flex justify-content-between align-items-center">
                     <div class="logo">
-                      <a href="index.html">
+                      <a href="index.php">
                         <div class="Logo">
                         <h1>TextCrunch <br>
                         </div>
